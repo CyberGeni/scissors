eeccb378d7d9a13903414b85294a5276c7185b82
Treasure Ajefu <treasureajefu@gmail.com>
2023-06-08 20:11:27 +0100

    looked a lil thin on mobile so i increased the width a lil bit

--- src/pages/static/Login/index.tsx
+++ src/pages/static/Login/index.tsx
@@ -118,7 +118,7 @@ function Login() {
                 <img className='w-full h-full object-cover' src={gradientBg} alt="" />
             </div>
 
-            <div className='flex flex-col justify-center text-center bg-gray-900 p-8 space-y-10 h-5/6 md:h-full w-4/5 md:w-full mx-auto my-auto rounded-md md:rounded-none'>
+            <div className='flex flex-col justify-center text-center bg-gray-900 p-8 space-y-10 h-11/12 md:h-full w-11/12 md:w-full mx-auto my-auto rounded-md md:rounded-none'>
                 <h1 className='text-gray-100 font-medium text-4xl tracking-tighter'>scissors sharp<span className='text-blue-700 text-6xl'>.</span></h1>
                 <h3 className='text-gray-200 font-medium text-2xl tracking-tight'>Log in to your account</h3>
                 <form className='max-w-sm 2xl:max-w-md w-full mx-auto text-gray-200 space-y-5' action="" onSubmit={login}>
